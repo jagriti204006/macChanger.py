431af186717a381cd315faeed373b1007712de86
Jagriti Malhotra <84565473+jagriti204006@users.noreply.github.com>
2022-07-13 17:52:21 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,29 @@
 import subprocess
+import optparse 
 
 # subprocess.run("ifconfig", shell=True)
 
-subprocess.run("ifconfig wlan0 down", shell=True)
-subprocess.run("ifconfig wlan0 hw ether C1:B1:23:82:67:AE", shell=True)
+def change(interface, mac):
+    subprocess.run("ifconfig "+interface+" down", shell=True)
+    subprocess.run("ifconfig "+interface+" hw ether "+mac, shell=True)
 
-subprocess.run("ifconfig wlan0 up", shell=True)
+    subprocess.run("ifconfig "+interface+" up", shell=True)
+
+def getArguments():
+    parser = optparse.OptionParser()
+  
+    parser.add_option("-i", "--interface", dest="interface", help="interface name of the target")
+    parser.add_option("-m", "--mac", dest="mac", help="new mac address you want to assign to the target")
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error("Please specify the interface name ")
+    if not options.mac:
+        parser.error("Please specify the new mac address ")
+
+    return options
+
+options = getArguments()
+change(options.interface, options.mac)
+
+print("Mac address changed")
